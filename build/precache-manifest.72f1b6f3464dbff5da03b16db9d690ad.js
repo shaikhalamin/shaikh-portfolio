e8a3a39333d8365f13d65cee906f07bb49c332b9
shaikh-alamin <shaikh.alamin@venturas-bd.com>
2022-01-27 23:04:27 +0600

    testing build from github

---
+++ build/precache-manifest.72f1b6f3464dbff5da03b16db9d690ad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1eedde757fc6d63da17e2188de23a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c5d3f829da2218ce1d9",
+    "url": "/static/css/2.1b0e7087.chunk.css"
+  },
+  {
+    "revision": "364fc267ee30dafcb13a",
+    "url": "/static/css/main.ec6fc5ed.chunk.css"
+  },
+  {
+    "revision": "7c5d3f829da2218ce1d9",
+    "url": "/static/js/2.40dc1bce.chunk.js"
+  },
+  {
+    "revision": "364fc267ee30dafcb13a",
+    "url": "/static/js/main.0c775d01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbc4931360e2dfdf55bb7bd423943d11",
+    "url": "/static/media/Resume_of_Shaikh_AL_Amin.fbc49313.pdf"
+  },
+  {
+    "revision": "b75cc2fcc2f3631a0a28684de17274d3",
+    "url": "/static/media/bgParticle.b75cc2fc.gif"
+  },
+  {
+    "revision": "f4d3b91d29307e141a366686916c5ed8",
+    "url": "/static/media/shaikh_alamin.f4d3b91d.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
